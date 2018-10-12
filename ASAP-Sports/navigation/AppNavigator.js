c6016a2728d5d7b42b2aca6bf67f2219d36d57e8
Kevin0115 <kevin.choi0115@gmail.com>
2018-10-12 13:21:44 -0700

    AppNavigator includes all screens; package.json updated to include react-navigation

--- ASAP-Sports/navigation/AppNavigator.js
+++ ASAP-Sports/navigation/AppNavigator.js
@@ -1,2 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+// Add screens here as needed
+import Homescreen from '../screens/Homescreen';
+import Gametype from '../screens/Gametype';
+import BrowseGames from '../screens/BrowseGames';
+import GameInfo from '../screens/GameInfo';
+import TimeDate from '../screens/TimeDate';
+import Location from '../screens/Location';
+import ReviewDetails from '../screens/ReviewDetails';
+import ConfirmMessage from '../screens/ConfirmMessage';
+
+export default createStackNavigator(
+	{
+		Homescreen: Homescreen,
+		Gametype: Gametype,
+		BrowseGames: BrowseGames,
+		GameInfo: GameInfo,
+		TimeDate: TimeDate,
+		Location: Location,
+		ReviewDetails: ReviewDetails,
+		ConfirmMessage: ConfirmMessage
+	},
+	{
+		initialRouteName: 'Homescreen', // This will be changed later to Login depending on conditions
+	}
+);
