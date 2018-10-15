dc242ddfb0f9fe2ffe1161e353b3624b9e39a5eb
Kevin0115 <kevin.choi0115@gmail.com>
2018-10-14 20:55:26 -0700

    Moved the hardcoded gametypes to different file

--- ASAP-Sports/screens/Gametype.js
+++ ASAP-Sports/screens/Gametype.js
@@ -2,70 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, FlatList, Image } from 'react-native';
 import AwesomeButton from 'react-native-really-awesome-button';
 
+import SportList from './SportList';
+
 export default class Gametype extends React.Component {
   render() {
     return (
       <View style={styles.gametype}>
         <View style={{flex: 6, alignItems: 'center', justifyContent: 'space-around'}}>
           <FlatList
-            data={[
-              {
-                key: 'Basketball',
-                image: require('../images/basketball.png'),
-              },
-              {
-                key: 'Volleyball',
-                image: require('../images/volleyball.png'),
-              },
-              {
-                key: 'Soccer',
-                image: require('../images/soccer.png'),
-              },
-              {
-                key: 'Baseball',
-                image: require('../images/baseball.png'),
-              },
-              {
-                key: 'Badminton',
-                image: require('../images/badminton.png'),
-              },
-              {
-                key: 'Football',
-                image: require('../images/football.png'),
-              },
-              {
-                key: 'Table Tennis',
-                image: require('../images/tabletennis.png'),
-              },
-              {
-                key: 'Tennis',
-                image: require('../images/tennis.png'),
-              },
-              {
-                key: 'Bouldering',
-                image: require('../images/bouldering.png'),
-              },
-              {
-                key: 'Skateboarding',
-                image: require('../images/skateboarding.png'),
-              },
-              {
-                key: 'Boxing',
-                image: require('../images/boxing.png'),
-              },
-              {
-                key: 'Wrestling',
-                image: require('../images/wrestling.png'),
-              },
-              {
-                key: 'Swimming',
-                image: require('../images/swimming.png'),
-              },
-              {
-                key: 'Ultimate Frisbee',
-                image: require('../images/frisbee.png'),
-              },
-            ]}
+            data={SportList}
             numColumns={2}
             renderItem={({item}) =>
               <AwesomeButton
@@ -109,7 +54,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'Helvetica',
   },
-
   listItem: {
     fontSize: 18,
   },
---
+++ ASAP-Sports/screens/SportList.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const SportList = [
+                    {
+                      key: 'Basketball',
+                      image: require('../images/basketball.png'),
+                    },
+                    {
+                      key: 'Volleyball',
+                      image: require('../images/volleyball.png'),
+                    },
+                    {
+                      key: 'Soccer',
+                      image: require('../images/soccer.png'),
+                    },
+                    {
+                      key: 'Baseball',
+                      image: require('../images/baseball.png'),
+                    },
+                    {
+                      key: 'Badminton',
+                      image: require('../images/badminton.png'),
+                    },
+                    {
+                      key: 'Football',
+                      image: require('../images/football.png'),
+                    },
+                    {
+                      key: 'Table Tennis',
+                      image: require('../images/tabletennis.png'),
+                    },
+                    {
+                      key: 'Tennis',
+                      image: require('../images/tennis.png'),
+                    },
+                    {
+                      key: 'Bouldering',
+                      image: require('../images/bouldering.png'),
+                    },
+                    {
+                      key: 'Skateboarding',
+                      image: require('../images/skateboarding.png'),
+                    },
+                    {
+                      key: 'Boxing',
+                      image: require('../images/boxing.png'),
+                    },
+                    {
+                      key: 'Wrestling',
+                      image: require('../images/wrestling.png'),
+                    },
+                    {
+                      key: 'Swimming',
+                      image: require('../images/swimming.png'),
+                    },
+                    {
+                      key: 'Ultimate Frisbee',
+                      image: require('../images/frisbee.png'),
+                    },
+                  ]
+
+export default SportList;
