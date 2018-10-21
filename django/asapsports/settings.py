2f25dacf621ce13650f18ceb1ba3f265463db75b
aidoraide <aidoraide@gmail.com>
2018-10-21 03:07:13 -0700

    some stuff working babyyyy

--- django/asapsports/settings.py
+++ django/asapsports/settings.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'asapsports',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
