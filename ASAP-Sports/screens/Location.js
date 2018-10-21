fd3fb1c481a338ea39add529297e599312f56dcf
Kevin0115 <kevin.choi0115@gmail.com>
2018-10-21 10:53:10 -0700

    Locations screens done; plaintext for now

--- ASAP-Sports/screens/Location.js
+++ ASAP-Sports/screens/Location.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import AwesomeButton from 'react-native-really-awesome-button';
 
 export default class Location extends React.Component {
   state = {
     location: '', // plaintext string
     locChosen: false,
+  };
+
+  _handleLocationChange = (location) => {
+    this.setState({location: location});
+    this.setState({locChosen: true});
   };
 
   _handleNextPress = () => {
@@ -28,12 +34,28 @@ export default class Location extends React.Component {
   render() {
     return (
       <View style={styles.location}>
-        <View>
-          <Button
-            title="Next"
-            color="blue"
-            onPress={this._handleNextPress}
+        <View style={styles.empty}/>
+        <View style={styles.input}>
+          <Text style={styles.textHeader}>Please Enter a Location</Text>
+          <TextInput
+            placeholder="Enter a location"
+            placeholderTextColor="#c9c9c9"
+            clearTextOnFocus={true}
+            style={styles.titleInput}
+            onChangeText={this._handleLocationChange}
+            value={this.state.location}
           />
+        </View>
+        <View style={styles.buttonContainer}>
+          <AwesomeButton
+            width={320}
+            height={60}
+            backgroundColor='#004e89'
+            backgroundDarker='#001a33'
+            onPress={() => this._handleNextPress()}
+          >
+            Next
+          </AwesomeButton>
         </View>
       </View>
 
@@ -48,4 +70,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  empty: {
+    flex: 2.5
+  },
+  textHeader: {
+    color: '#707070',
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 15,
+  },
+  input: {
+    flex: 3.5,
+    width: '90%',
+    alignItems: 'center',
+  },
+  titleInput: {
+    fontSize: 20,
+    height: 48,
+    width: '90%',
+    borderColor: '#c9c9c9',
+    borderWidth: 2,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
