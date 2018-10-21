52637a2f3d6e374dfce8062596beb36c50bc3974
Harpaul Gill <harpaul_gill@hotmail.com>
2018-10-21 13:16:01 -0700

    added year to date

--- ASAP-Sports/screens/TimeDate.js
+++ ASAP-Sports/screens/TimeDate.js
@@ -63,6 +63,7 @@ export default class TimeDate extends React.Component {
             weekday: 'long',
             month: 'long',
             day: 'numeric',
+            year: 'numeric',
           }
         )
       ),
