082e85e7361eed06ab50e15d89bafe21748a2d63
aidoraide <aidoraide@gmail.com>
2018-11-10 17:01:52 -0800

    more changes to init_app.sh

--- django/prod/init_app.sh
+++ django/prod/init_app.sh
@@ -11,6 +11,7 @@ sudo mkdir /webapps/$APP_NAME
 sudo useradd --system --gid webapps --home /webapps/$APP_NAME $APP_NAME
 echo 'Enter a password for user '$APP_NAME':'
 sudo passwd $APP_NAME
+# TODO Double check chowns
 sudo chown -R $APP_NAME:users /webapps/$APP_NAME
 cd /webapps/$APP_NAME
 sudo git clone https://github.com/Kevin0115/ASAP-Sports
@@ -20,16 +21,22 @@ sudo $VENV_BIN/pip3.6 install -r requirements.txt
 
 # Create Nginx/systemd conf files and
 sudo $VENV_BIN/python3.6 production.py -a $APP_NAME -d $DOMAIN_NAME
-sudo systemctl daemon-reload
-service nginx restart
-sudo systemctl start $APP_NAME
 
+# TODO Double check chowns
+# Set file/directory permissions
 sudo chown -R $APP_NAME:users /webapps/$APP_NAME
 sudo chown -R $APP_NAME:webapps $DJANGO
 sudo chmod -R g+w $DJANGO
 
+# Create DB
 sudo -u postgres createuser $APP_NAME
 sudo -u postgres createdb --owner $APP_NAME $APP_NAME
+sudo -u $APP_NAME psql -f "prod/createdb.sql"
+
+# Now that the files are all setup and
+sudo systemctl daemon-reload
+sudo service nginx restart
+sudo systemctl start $APP_NAME
 
 # Only needed if we use ORM
 # sudo -u $APP_NAME $VENV_BIN/python $DJANGO/$APP_NAME/manage.py migrate
