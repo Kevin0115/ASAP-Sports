c59b61fccce187ed98b547845896e390c7f42aeb
aidoraide <aidoraide@gmail.com>
2018-11-17 21:50:10 -0800

    Update utils.py

--- django/test/utils.py
+++ django/test/utils.py
@@ -15,17 +15,3 @@ def get_connection(): # TODO change to connect to test DB instead
                             host=DB_INFO.get('HOST'),
                             port=DB_INFO.get('PORT'))
 
-
-
-def insert_user(conn, fb_id, first, last, fb_access_token,
-                 profile_pic_url, asap_access_token):
-    query = """
-        insert into users (fb_id, first, last, fb_access_token, 
-        profile_pic_url, asap_access_token)
-          values (%(fb_id)s, %(first)s, %(last)s, %(fb_access_token)s, 
-          %(profile_pic_url)s, %(asap_access_token)s)
-    """
-    with conn.cursor() as curs:
-        print(locals())
-        curs.execute(query, locals())
-
